5baec69b9875d551f88a868b5cba3d6b56e842a1
sachinmishra10 <sachin.mishra10@gmail.com>
2018-12-02 13:58:57 +0530

    Initial commit

---
+++ MvcApplication1/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Employee
+    {
+        public string   EmployeeName { get; set; }
+        public string EmployeeDepartment { get; set; }
+    }
+}
